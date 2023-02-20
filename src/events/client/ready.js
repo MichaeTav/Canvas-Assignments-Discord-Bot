59b88a6173b08b32218d5d5cee0aaae6fbdaa18d
MichaeTav <m.tavera@hotmail.com>
2023-02-20 07:23:16 -0800

    Added readme

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -11,12 +11,11 @@ module.exports = {
     console.log(`READY ${client.user.tag} is logged in and online`);
     //Every Sunday at 8am "0 12 * * 0"
     const weeklyJob = new CronJob(
-      "0 12 * * 0",
+      "15 12 * * 0",
       async function () {
         const embed = await weekly.weeklyUpdate();
         const channel = client.channels.cache.find(
-          //(channel) => channel.name === `${channelName}`
-          (channel) => channel.name === `this-is-a-test`
+          (channel) => channel.name === `${channelName}`
         );
         channel.send({ embeds: [embed] });
       },
